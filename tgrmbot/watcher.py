79140387a85152de265c580403accb62613432d9
Victor Antonovich <v.antonovich@gmail.com>
2017-06-21 12:29:16 +0300

    Refactor application reviews watcher

--- tgrmbot/watcher.py
+++ tgrmbot/watcher.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 Created on 20-Feb-2017
 
@@ -12,9 +13,10 @@ from tgrmbot.util import sleep
 
 from tgrmbot.googleplay.market import MarketSession, RequestError
 
-RELOGIN_TIMEOUT = 60 # 1m
+RELOGIN_TIMEOUT = 60  # 1m
 
 REVIEW_TEMPLATE_NAME = 'review.md'
+
 
 class GpReviewsWatcher(service.Service):
     '''
@@ -35,115 +37,127 @@ class GpReviewsWatcher(service.Service):
         self.gp_langs = gp_langs
         self._d_poll = None
 
-
     def startService(self):
         self._bot = self.parent.getServiceNamed('tgrmbot_bot')
-        reactor.callLater(3, self._run)
-
+        reactor.callLater(3, self._run)  # @UndefinedVariable
 
     @defer.inlineCallbacks
     def _run(self):
         # poll cycle loop
         session = MarketSession(self.android_id)
-        while reactor.running:
+        while reactor.running:  # @UndefinedVariable
             if not session.loggedIn:
                 log.msg('Authorizing Google Play session...')
                 try:
                     yield session.login(self.gp_login, self.gp_password)
+                    yield sleep(0)  # switch to main thread
                 except Exception as e:
                     log.err(e, 'Can\'t authorize Google Play session')
                     # delay before next login try
                     yield sleep(RELOGIN_TIMEOUT)
                     continue
-            yield sleep(0) # switch to main thread
-            log.msg('Checking for new reviews...')
-            apps = yield self.db.get_apps()
-            for app in apps:
-                app_id, app_name, app_desc = app
-                try:
-                    # get last gp_reviews for an application
-                    reviews = []
-                    for gp_lang in self.gp_langs:
-                        gp_reviews = yield session.getReviews(appid=app_name, lang=gp_lang)
-                        yield sleep(0) # switch to main thread
-                        for gp_review in gp_reviews:
-                            review_author_id = gp_review.get('commentId')
-                            review_author_name = gp_review.get('authorName')
-                            review_timestamp = gp_review.get('timestampMsec')
-                            review_rating = gp_review.get('starRating')
-                            review_comment = gp_review.get('comment')
-                            # check for review with same author id
-                            db_reviews = yield self.db.get_reviews(app_id, review_author_id)
-                            if not db_reviews:
-                                # new review found, will add to database and notify
-                                log.msg('Found new review for %s from author: %s' % (app_name, review_author_id,))
-                                yield self.db.add_review(app_id, review_author_id, review_author_name,
-                                                         review_timestamp, review_rating, review_comment,
-                                                         gp_lang)
-                                reviews.append({'timestamp': review_timestamp,
-                                                'version': gp_review.get('documentVersion'),
-                                                'device': gp_review.get('deviceName'),
-                                                'rating': review_rating,
-                                                'title': gp_review.get('title'),
-                                                'comment': review_comment,
-                                                'lang': gp_lang})
-                            else:
-                                # review with given author ID already seen, check for changes
-                                review_id, _app_id, _author_id, _author_name, _timestamp, \
-                                    old_review_rating, old_review_comment, _lang = db_reviews[0]
-                                if (review_rating != old_review_rating) or (review_comment != old_review_comment):
-                                    # review changed, will update database and notify
-                                    log.msg('Found changed review for %s from author: %s' % \
-                                            (app_name, review_author_id,))
-                                    yield self.db.update_review(review_id, review_rating, review_comment)
-                                    reviews.append({'timestamp': review_timestamp,
-                                                    'version': gp_review.get('documentVersion'),
-                                                    'device': gp_review.get('deviceName'),
-                                                    'rating': review_rating,
-                                                    'title': gp_review.get('title'),
-                                                    'comment': review_comment,
-                                                    'old_rating': old_review_rating,
-                                                    'old_comment': old_review_comment,
-                                                    'lang': gp_lang})
-                        if gp_lang != self.gp_langs[-1]:
-                            # delay before check next gp_lang
-                            yield sleep(self.poll_delay)
-                    # notify about new gp_reviews all related chats
-                    if reviews:
-                        # sort gp_reviews by creation time before notification
-                        reviews.sort(key = lambda k: k.get('timestamp'))
-                        # notify all related chats
-                        watchers = yield self.db.get_watchers(app_id)
-                        if watchers:
-                            chat_ids = [chat_id for _watcher_id, _app_id, chat_id in watchers]
-                            messages = [self.templateRenderer.render(REVIEW_TEMPLATE_NAME,
-                                                                     app={'name': app_name, 'desc': app_desc},
-                                                                     review=review)
-                                        for review in reviews]
-                            for chat_id in chat_ids:
-                                yield self._bot.send_messages(chat_id, messages)
+            yield self._check_app_reviews(session)
 
-                except RequestError as re:
-                    if re.err_code == 401:
-                        log.err(re, 'Access denied while checking for new gp_reviews for an application %s' % app_name)
-                        session.loggedIn = False
-                        yield sleep(RELOGIN_TIMEOUT)
-                        break
-                    log.err(re, 'Can\'t request new gp_reviews for an application %s' % app_name)
-                except Exception as e:
-                    log.err(e, 'Error while checking for new gp_reviews for an application %s' % app_name)
-                if app != apps[-1]:
-                    # delay before check next application
-                    yield sleep(self.poll_delay)
-
-            # delay before next applications poll cycle
-            self._d_poll = sleep(self.poll_period)
+    @defer.inlineCallbacks
+    def _check_app_reviews(self, session):
+        log.msg('Checking for new application reviews...')
+        apps = yield self.db.get_apps()
+        for app in apps:
+            app_id, app_name, app_desc = app
             try:
-                yield self._d_poll
-            except defer.CancelledError:
-                # pause canceled
-                pass
+                # get new reviews for an application
+                app_reviews = []
+                for gp_lang in self.gp_langs:
+                    lang_reviews = yield self._get_app_reviews(session, app_id, app_name, gp_lang)
+                    app_reviews.extend(lang_reviews)
+                    if gp_lang != self.gp_langs[-1]:
+                        # delay before check next gp_lang
+                        yield sleep(self.poll_delay)
+                if app_reviews:
+                    # sort app_reviews by creation time before notification
+                    app_reviews.sort(key=lambda k: k.get('timestamp'))
+                    # notify all related chats about new application reviews
+                    yield self._notify_watchers(app_id, app_name, app_desc, app_reviews)
+            except RequestError as re:
+                if re.err_code == 401:
+                    log.err(re, 'Access denied while checking for new reviews ' +
+                            'for an application %s' % app_name)
+                    session.loggedIn = False
+                    yield sleep(RELOGIN_TIMEOUT)
+                    break
+                log.err(re, 'Can\'t request new reviews for an application %s' % app_name)
+            except Exception as e:
+                log.err(e, 'Error while checking for new reviews for an application %s' % app_name)
+            if app != apps[-1]:
+                # delay before check next application
+                yield sleep(self.poll_delay)
 
+        # delay before next applications poll cycle
+        self._d_poll = sleep(self.poll_period)
+        try:
+            yield self._d_poll
+        except defer.CancelledError:
+            # pause canceled
+            pass
+
+    @defer.inlineCallbacks
+    def _get_app_reviews(self, session, app_id, app_name, gp_lang):
+        app_reviews = []
+        gp_reviews = yield session.getReviews(appid=app_name, lang=gp_lang)
+        yield sleep(0)  # switch to main thread
+        for gp_review in gp_reviews:
+            review_author_id = gp_review.get('commentId')
+            review_author_name = gp_review.get('authorName')
+            review_timestamp = gp_review.get('timestampMsec')
+            review_rating = gp_review.get('starRating')
+            review_comment = gp_review.get('comment')
+            # check for review with same author id
+            db_reviews = yield self.db.get_reviews(app_id, review_author_id)
+            if not db_reviews:
+                # new review found, will add to database and notify
+                log.msg('Found new review for %s from author: %s' % (app_name, review_author_id,))
+                yield self.db.add_review(app_id, review_author_id, review_author_name,
+                                         review_timestamp, review_rating, review_comment, gp_lang)
+                app_reviews.append({'timestamp': review_timestamp,
+                                    'version': gp_review.get('documentVersion'),
+                                    'device': gp_review.get('deviceName'),
+                                    'rating': review_rating,
+                                    'title': gp_review.get('title'),
+                                    'comment': review_comment,
+                                    'lang': gp_lang})
+            else:
+                # review with given author ID already seen, check for changes
+                review_id, _app_id, _author_id, _author_name, _timestamp, \
+                    old_review_rating, old_review_comment, _lang = db_reviews[0]
+                if (review_rating != old_review_rating) or (review_comment != old_review_comment):
+                    # review updated, will update database and notify
+                    log.msg('Found updated review for %s from author: %s' %
+                            (app_name, review_author_id,))
+                    yield self.db.update_review(review_id, review_rating, review_comment)
+                    app_reviews.append({'timestamp': review_timestamp,
+                                        'version': gp_review.get('documentVersion'),
+                                        'device': gp_review.get('deviceName'),
+                                        'rating': review_rating,
+                                        'title': gp_review.get('title'),
+                                        'comment': review_comment,
+                                        'old_rating': old_review_rating,
+                                        'old_comment': old_review_comment,
+                                        'lang': gp_lang})
+
+        defer.returnValue(app_reviews)
+
+    @defer.inlineCallbacks
+    def _notify_watchers(self, app_id, app_name, app_desc, app_reviews):
+        # notify all related chats about new application reviews
+        watchers = yield self.db.get_watchers(app_id)
+        if watchers:
+            chat_ids = [chat_id for _watcher_id, _app_id, chat_id in watchers]
+            messages = [self.templateRenderer.render(REVIEW_TEMPLATE_NAME,
+                                                     app={'name': app_name, 'desc': app_desc},
+                                                     review=review)
+                        for review in app_reviews]
+            for chat_id in chat_ids:
+                yield self._bot.send_messages(chat_id, messages)
 
     @defer.inlineCallbacks
     def watch(self, chat_id, app_name, app_desc):
@@ -162,7 +176,6 @@ class GpReviewsWatcher(service.Service):
         # force next poll cycle
         self.poll()
         defer.returnValue(True)
-
 
     @defer.inlineCallbacks
     def unwatch(self, chat_id, app_name):
@@ -187,12 +200,10 @@ class GpReviewsWatcher(service.Service):
 
         defer.returnValue((app_name, app_desc))
 
-
     @defer.inlineCallbacks
     def watched_apps(self, chat_id):
         apps = yield self.db.get_watched_apps(chat_id)
         defer.returnValue(apps)
-
 
     def poll(self):
         if self._d_poll is not None:
