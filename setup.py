b01720654e0f5f11117a71356b20c49c39a54205
Victor Antonovich <v.antonovich@gmail.com>
2023-10-30 17:25:43 +0300

    Version 0.3.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
                               'locales/ru_RU/LC_MESSAGES/messages.mo']},
     zip_safe=False,
     include_package_data=True,
-    version='0.3.0',
+    version='0.3.2',
 
     url='https://github.com/3cky/gp-reviews-tgrm-bot',
     author='Victor Antonovich',
